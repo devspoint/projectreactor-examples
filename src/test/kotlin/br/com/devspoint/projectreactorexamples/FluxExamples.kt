13c7c5251804e9d0760aeb50d733564fc1226479
Felipe Formentin <felipe.formentin@mercadolivre.com>
2021-09-23 19:23:28 -0300

    Creating Mono

---
+++ src/test/kotlin/br/com/devspoint/projectreactorexamples/FluxExamples.kt
@@ -0,0 +1,34 @@
+package br.com.devspoint.projectreactorexamples
+
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.core.publisher.Mono.justOrEmpty
+import java.util.*
+
+class FluxExamples {
+
+    @Test
+    fun `creating flux`() {
+        val fluxJust = Flux.just(
+            System.currentTimeMillis(),
+            System.currentTimeMillis(),
+            System.currentTimeMillis()
+        )
+
+        val fluxEmpty = Flux.empty<String>()
+
+        val fluxDefer = Flux.defer { Flux.just(System.currentTimeMillis()) }
+
+        println("Assinando eventos criado com o Just")
+        fluxJust.subscribe(System.out::println)
+        Thread.sleep(500L)
+
+        println("Assinando eventos criado com o Defer")
+        fluxDefer.subscribe(System.out::println)
+        Thread.sleep(500L)
+
+        println("Assinando eventos criado com o Empty e JustOrEmpty")
+        fluxEmpty.subscribe { println("não passei aqui") }
+    }
+}
---
+++ src/test/kotlin/br/com/devspoint/projectreactorexamples/MonoExamples.kt
@@ -0,0 +1,30 @@
+package br.com.devspoint.projectreactorexamples
+
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
+import java.util.*
+
+class MonoExamples {
+
+    @Test
+    fun `creating mono`() {
+        val monoJust = Mono.just(System.currentTimeMillis())
+        val monoEmpty = Mono.empty<String>()
+        val monoJustOrEmpty = Mono.justOrEmpty(Optional.empty<String>())
+        val monoDefer = Mono.defer { Mono.just(System.currentTimeMillis()) }
+
+        println("Assinando eventos criado com o Just")
+        monoJust.subscribe(System.out::println)
+        Thread.sleep(500L)
+        monoJust.subscribe(System.out::println)
+
+        println("Assinando eventos criado com o Defer")
+        monoDefer.subscribe(System.out::println)
+        Thread.sleep(500L)
+        monoDefer.subscribe(System.out::println)
+
+        println("Assinando eventos criado com o Empty e JustOrEmpty")
+        monoEmpty.subscribe { println("não passei aqui") }
+        monoJustOrEmpty.subscribe { println("não passei aqui") }
+    }
+}
