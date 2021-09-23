46cc9e58dd76ecbcebd5387fbcba69c39bdaab59
Felipe Formentin <felipe.formentin@mercadolivre.com>
2021-09-23 17:10:14 -0300

    Initial Structure

---
+++ src/main/kotlin/br/com/devspoint/projectreactorexamples/ProjectreactorExamplesApplication.kt
@@ -0,0 +1,11 @@
+package br.com.devspoint.projectreactorexamples
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectreactorExamplesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectreactorExamplesApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/devspoint/projectreactorexamples/ProjectreactorExamplesApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.devspoint.projectreactorexamples
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectreactorExamplesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
